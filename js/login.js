de3d521a4fa02a7006caf904454abb14e393445f
pedroantuness <RM551409@fiap.com.br>
2023-05-25 11:37:31 -0300

    Criado uma pagina para o sucesso do login

--- js/login.js
+++ js/login.js
@@ -28,93 +28,112 @@ const usuario2 = {
 //console.log(usuario1.tokenzinho);
 */
 
-// //CRIANDO OBJETOS
-// let listaDeUsuarios = [
-//     {
-//         nomeCompleto : "Denden da Silva",
-//         nomeUsuario : "denden",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Gersu da Silva",
-//         nomeUsuario : "gege",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "José da Silva",
-//         nomeUsuario : "jose",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Paulo das Couves",
-//         nomeUsuario : "paula",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Mary Help",
-//         nomeUsuario : "mary",
-//         senhaUsuario : "123456"
-//     },
-//     {
-//         nomeCompleto : "Pedro Silva",
-//         nomeUsuario : "pedro",
-//         senhaUsuario : "123456"
-//     },
-// ];
+//CRIANDO OBJETOS
+let listaDeUsuarios = [
+    {
+        nomeCompleto : "Denden da Silva",
+        nomeUsuario : "denden",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Gersu da Silva",
+        nomeUsuario : "gege",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "José da Silva",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Paulo das Couves",
+        nomeUsuario : "paula",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Mary Help",
+        nomeUsuario : "mary",
+        senhaUsuario : "123456"
+    },
+    {
+        nomeCompleto : "Pedro Silva",
+        nomeUsuario : "pedro",
+        senhaUsuario : "123456"
+    },
+];
 
 //GUARDAR A LISTA DE OBJETOS NO LOCAL-STORAGE
-localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
+localStorage.setItem( "listaUser",  JSON.stringify(listaDeUsuarios));
 
-addEventListener("click", (evt) => {
- 
+addEventListener("click", (evt)=>{
     const inputUser = document.querySelector("#idUser");
     const inputPass = document.querySelector("#idPass");
 
+    //CRIANDO O OBJETO USER-LOGADO
+    const usuarioLogado = {
+        nomeUsuarioLogado : inputUser.value,
+        senhaUsuarioLogado : inputPass.value,
+    }
+
+    //CRIANDO O OBJETO USUARIO-VALIDADO
+    let usuarioValidado = [];
+
     if(evt.target.id == "btnSubmit"){
         
-        try{
-            //RECUPERAR A LISTA DE USUARIOS DO LOCALSTORAGE
+        try {
+          
+            //Recuperar a lista de usuários do LocalStorage
             let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
 
-            listaDeUsuarios.forEach((usuario) => {
-    
-                if(inputUser.value == usuario.nomeUsuario && inputPass.value == usuario.senhaUsuario){
-                    throw "Validado!";            
+            listaDeUsuarios.forEach((usuario)=>{
+                
+                if(usuarioLogado.nomeUsuarioLogado == usuario.nomeUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario){
+                    usuarioValidado = usuario;
+                    throw "VALIDADO";
                 }
             });
-
-            throw "Não Validado";
-
-        }
-        catch (msg) {
             
+            throw "NÃO VALIDADO";
+
+        } catch (msg) {
             const msgError = document.querySelector("#msgError");
+            if(msg == "VALIDADO"){
+                msgError.setAttribute("style","color:#00ff00;");
+                msgError.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} efetuou o login com Sucesso!</strong></span>`;
 
-            if(msg == "Validado!") {
-                msgError.setAttribute("style", "color: #00ff00");
-                msgError.innerHTML = "<span><strong>Login efetuado com Sucesso!</strong></span>";
+                //ADICIONANDO O OBJETO USUARIO-VALIDADO
+                localStorage.setItem("user-validado", JSON.stringify(usuarioValidado));
 
-                //REDIRECT ESPERA 3 SEGUNDOS ANTES DE REDIRECIONAR
-                setTimeout(function(){
+                //CRIANDO O TOKEN DE AUTENTICAÇÃO
+                const token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2); //substring RETORNA A STRING A PARTIR DA POSIÇÃO QUE DEFINIRMOS
+
+                //ADICIONADO O TOKEN NO LOCAL-STORAGE
+                localStorage.setItem("user-token", token);
+                
+                //Redirect : Espera 3 segundos antes de redirecionar
+                setTimeout( function(){
                     window.location.href = "../pages/sucesso.html";
                 }, 3000);
-            }
-            else {
-                msgError.setAttribute("style", "color: #ff0000");
-                msgError.innerHTML = "<span><strong>Usuário ou Senha incorreto!</strong></span>";
+
+            }else{
+                msgError.setAttribute("style","color:#ff0000;");
+                msgError.innerHTML = "<span><strong>Usuário ou senha inválidos!</strong></span>";
             }
         }
-    }
-    else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
 
+//DESAFIO:
+//Troque o ícone do olho aberto assim que o usuário clicar para ver a senha pelo ícone de olho cortado.
+
+    }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
+        //Mostrando a senha!
         if(inputPass.getAttribute("type") == "password"){
-            evt.target.setAttribute("class", "fa fa-eye-slash");
-            inputPass.setAttribute("type", "text");
+            evt.target.setAttribute("class","fa fa-eye-slash");
+            inputPass.setAttribute("type","text");
+        }else{
+        //Ocultando a senha!
+        evt.target.setAttribute("class","fa fa-eye");
+            inputPass.setAttribute("type","password");
         }
-        else{
-            evt.target.setAttribute("class", "fa fa-eye");
-            inputPass.setAttribute("type", "password");
-        }
-
+        
     }
 });
